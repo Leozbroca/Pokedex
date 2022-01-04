a194381d306bc7a837415edaaff9fb428267e1ff
Leozbroca <leo.brokka@gmail.com>
2022-01-04 14:12:03 -0300

    bugfixes

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import {React} from 'react'
 import Router from './routes/Router';
 import GlobalStyle from './constants/GlobalStyle';
@@ -32,7 +10,6 @@ function App() {
       <GlobalStyle/>
       <Header/>
       <Router/>
->>>>>>> master
     </div>
   );
 }
